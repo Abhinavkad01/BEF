4078b3b9fae03aa58e142fbbd5629a29d431fc92
Abhinavkad01 <109024565+Abhinavkad01@users.noreply.github.com>
2025-03-10 19:37:35 +0530

    Update Untitled51.py

--- Untitled51.py
+++ Untitled51.py
@@ -57,7 +57,7 @@ if page == "Signup":
         else:
             st.error("⚠️ Please fill in all fields and agree to the terms.")
     # Add images only under the Signup Page
-    signup_images = ["Screenshot 2025-03-10 122106.png", "Screenshot 2025-03-10 124543.png"]
+    signup_images = ["Screenshot 2025-03-10 122106.png", "Screenshot 2025-03-10 124543.png","Screenshot 2025-03-10 193559.png"]
     for image in signup_images:
         if os.path.exists(image):
             st.image(image, use_container_width=True)
